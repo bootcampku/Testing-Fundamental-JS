efa1391ba943a8d8f73bf20071d92f057576df87
Bootcampku <bootcampku@gmail.com>
2021-10-13 01:38:08 +0700

    encapsulate with a test function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const { square, sum } = require('./math');
 const expect = require('./expect');
 
-let result = square(2);
-let expected = 4;
+test('test fungsi kuadrat', () => {
+  let result = square(2);
+  let expected = 2;
 
-expect(result).toBe(expected);
+  expect(result).toBe(expected);
+});
 
-result = sum(2, 3);
-expected = 5;
+test('test fungsi pertambahan', () => {
+  result = sum(2, 3);
+  expected = 5;
 
-expect(result).toBe(expected);
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✅ ${title} berhasil. \n`);
+  } catch (err) {
+    console.error(`❌ ${title} gagal.`);
+    console.error(`${err.message} \n`);
+  }
+}
